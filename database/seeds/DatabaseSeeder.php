0fed0a769621f51fbab20c627f9b849c9687baa5
Betim <betim.ademi@gmail.com>
2018-12-20 00:12:58 +0100

    Seeding part for Product and Reviews

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -6,11 +6,14 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
-     *
+     * MBUSH DATABAZEN
      * @return void
      */
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        factory(App\Model\Product::class,50)->create();
+        factory(App\Model\Review::class,300)->create();
     }
 }
