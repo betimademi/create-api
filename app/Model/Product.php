05507764da90839c93fcf84cab4f53db6d820066
Betim <betim.ademi@gmail.com>
2018-12-20 01:49:56 +0100

    Product Resource created

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    // Creating Database Tables Relationship/   One Product can have many reviews/  one to many
+
+    public function reviews(){
+
+        return $this->hasMany(Review::class);
+
+    }
+
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    // HERE WE CREATE ALSO THE RELATIONSHIP BETWEEN PRODUCTS AND REVIEWS AND WE TELL THAT THIS PRODUCT BELONGS TO REVIEWS
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+
+    }
+
 }
